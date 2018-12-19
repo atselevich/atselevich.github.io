34143cb49840bab2ca09449030810ef7d3e74552
atselevich <atselevich@a-g.com>
2018-12-19 13:04:55 -0500

    it works!

--- project/js/scripts.js
+++ project/js/scripts.js
@@ -1,3 +1,6 @@
+let baseHeight = 200;
+let waveDelta = 10;
+
 $(document).ready(function () {
     //http://magicseaweed.com/developer/forecast-api    
     // Key:  e035ec3907acbee73a5eea8ba2f3e2fc
@@ -9,17 +12,30 @@ $(document).ready(function () {
     //get height, speed based on magicseaweed data for spot. Add some randomness and variation based on min/max breaking height.
     //timestamps are in unix encoding, convert at https://www.unixtimestamp.com/index.php    
 
-    $('#wave1').drawWaves({
-        //height: .5
+
+
+    $('#wave4').drawWaves({
+        height: baseHeight,
+        waveDelta: waveDelta,
         speed: .15,
         color: '#1F538F'
     });
-    $('#wave2').drawWaves({
+    $('#wave3').drawWaves({
+        height: baseHeight - 5,
+        waveDelta: waveDelta,
         speed: .25,
         color: '#407DB3'
     });
-    $('#wave3').drawWaves({
+    $('#wave2').drawWaves({
+        height: baseHeight - 10,
+        waveDelta: waveDelta,
         speed: .45,
+        color: '#b4cee4'
+    })
+    $('#wave1').drawWaves({
+        height: baseHeight - 15,
+        waveDelta: waveDelta,
+        speed: .65,
         color: '#6DA2CC'
     })
 
@@ -39,19 +55,75 @@ $(document).ready(function () {
         });
     });
 
-    $(".spot").click(function(){
-        
-        let spotId = $(this).data("mswid");       
+    $(".spot").click(function () {
 
-        $.ajax("http://magicseaweed.com/api/e035ec3907acbee73a5eea8ba2f3e2fc/forecast/?spot_id=" + spotId,{
-            success: function(data){
-                alert(data);
+        let selected = getSelected();
+        if(selected.length)
+        {
+            selected.removeAttr("selected")
+        }
+
+        let spotId = $(this).data("mswid");
+        $(this).attr("selected", true)
+
+        $.ajax("http://magicseaweed.com/api/e035ec3907acbee73a5eea8ba2f3e2fc/forecast/?spot_id=" + spotId, {
+            success: function (data) {
+                console.log(data);
+                redrawWaves(data, 0)
             }
-        } )
-        
+        })
+
         $(".menu").slideToggle("slow", function () {
             $(".cross").hide();
             $(".hamburger").show();
         });
     })
 });
+
+function getSelected(){
+    return $('.spot[selected="selected"]');
+}
+
+function fillConditionInfo(timeBlock) {
+    let name = getSelected().text();
+
+    $("#spot-name").text(name);
+    $("#spot-combined-height").text("Combined Height: " + timeBlock.swell.components.combined.height + "ft");
+    $("#spot-primary-height").text("Primary Height: " + timeBlock.swell.components.primary.height + "ft");
+    $("#spot-secondary-height").text("Secondary Height: " + timeBlock.swell.components.secondary.height + "ft");
+    $("#spot-tertiary-height").text("Tertiary Height: " + timeBlock.swell.components.tertiary.height + "ft");
+}
+
+function redrawWaves(mswData, index) {
+
+    let heightMultiplier = 50;
+    let periodMultiplier = .1;
+    let timeBlock = mswData[index];
+
+    fillConditionInfo(timeBlock);
+
+    $('#wave4').drawWaves({
+        height: baseHeight,
+        waveDelta: timeBlock.swell.components.combined.height * heightMultiplier,
+        speed: timeBlock.swell.components.combined.period * periodMultiplier,
+        color: '#1F538F'
+    });
+    $('#wave3').drawWaves({
+        height: baseHeight - 5,
+        waveDelta: timeBlock.swell.components.primary.height * heightMultiplier,
+        speed: timeBlock.swell.components.primary.period * periodMultiplier,
+        color: '#407DB3'
+    });
+    $('#wave2').drawWaves({
+        height: baseHeight - 10,
+        waveDelta: timeBlock.swell.components.secondary.height * heightMultiplier,
+        speed: timeBlock.swell.components.secondary.period * periodMultiplier,
+        color: '#b4cee4'
+    })
+    $('#wave1').drawWaves({
+        height: baseHeight - 15,
+        waveDelta: timeBlock.swell.components.tertiary.height * heightMultiplier,
+        speed: timeBlock.swell.components.tertiary.period * periodMultiplier,
+        color: '#6DA2CC'
+    })
+}
